dc3e565fd5e0172ae00af2c7f80de23267246e9d
mnguyenn19 <mnguyenn19@gmail.com>
2021-04-11 13:59:56 -0500

    Changed database

--- app/src/main/java/com/example/ubsandroidapplication/MainActivity.java
+++ app/src/main/java/com/example/ubsandroidapplication/MainActivity.java
@@ -4,7 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+
+    //Firebase Database
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    //In each section, where the button is for you to save all the data in the database, put this there:
+//    rootNode = FirebaseDatabase.getInstance();
+//    reference = rootNode.getReference("Username"); // this changes based on where you want to place the data in firebase. Look in firebase.
+
+//    Class class here.
+
+//    reference.setValue("");
+
+//    Have to create a class for each one too in their specific categories.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

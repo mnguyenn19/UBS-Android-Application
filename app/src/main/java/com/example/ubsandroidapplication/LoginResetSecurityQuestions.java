c1ee9d907880427c70f6a4d230f77d894f2a4478
JackelynM <jackelynmacias81@gmail.com>
2021-04-13 18:55:33 -0500

    Added Login Reset Security Questions Page

---
+++ app/src/main/java/com/example/ubsandroidapplication/LoginResetSecurityQuestions.java
@@ -0,0 +1,15 @@
+package com.example.ubsandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+public class LoginResetSecurityQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_reset_security_questions);
+    }
+}

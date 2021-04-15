17b4de8c1ce601527cf5912f87c3b84b6f783aed
mnguyenn19 <mnguyenn19@gmail.com>
2021-04-15 11:02:21 -0500

    Updated Database added Exchange layout

--- app/src/main/java/com/example/ubsandroidapplication/Exchange.java
+++ app/src/main/java/com/example/ubsandroidapplication/Exchange.java
@@ -1,17 +1,28 @@
 package com.example.ubsandroidapplication;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class Exchange extends AppCompatActivity {
+public class Exchange extends AppCompatActivity implements View.OnClickListener{
 
+    ColorStateList attribute;
+    TextView home;
+    TextView createpost;
+    TextView edit;
+    TextView choice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +30,57 @@ public class Exchange extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        home = findViewById(R.id.home);
+        createpost = findViewById(R.id.createpost);
+        edit = findViewById(R.id.edit);
+        home.setOnClickListener(this);
+        createpost.setOnClickListener(this);
+        edit.setOnClickListener(this);
+        choice = findViewById(R.id.choice);
+
+        attribute = createpost.getTextColors();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.exchange_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int change = item.getItemId();
+        if (change == R.id.activity_options)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.home)
+        {
+            choice.animate().x(0).setDuration(100);
+            home.setTextColor(Color.WHITE);
+            createpost.setTextColor(attribute);
+            edit.setTextColor(attribute);
+        }
+        else if (v.getId() == R.id.createpost)
+        {
+            home.setTextColor(attribute);
+            createpost.setTextColor(Color.WHITE);
+            edit.setTextColor(attribute);
+            int size = createpost.getWidth();
+            choice.animate().x(size).setDuration(100);
+        }
+        else if (v.getId() == R.id.edit)
+        {
+            home.setTextColor(attribute);
+            createpost.setTextColor(Color.WHITE);
+            edit.setTextColor(attribute);
+            int size = createpost.getWidth()*2;
+            choice.animate().x(size).setDuration(100);
+        }
     }
 }

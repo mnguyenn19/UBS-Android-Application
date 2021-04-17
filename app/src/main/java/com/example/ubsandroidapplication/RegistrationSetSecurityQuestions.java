18b1f6b2ccb9282501b9560eb2af40c0cc092fd8
JackelynM <jackelynmacias81@gmail.com>
2021-04-17 11:39:00 -0500

    Added Registration Page and Set Security Questions Page

---
+++ app/src/main/java/com/example/ubsandroidapplication/RegistrationSetSecurityQuestions.java
@@ -0,0 +1,14 @@
+package com.example.ubsandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationSetSecurityQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_set_security_questions);
+    }
+}

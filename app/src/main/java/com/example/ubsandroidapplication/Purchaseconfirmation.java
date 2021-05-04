da88030c1714c271b12af0cc73308e2b5ea3d22f
adityamishra <aditya.mishra@mavs.uta.edu>
2021-05-04 12:28:03 -0500

    Updated.

--- app/src/main/java/com/example/ubsandroidapplication/Purchaseconfirmation.java
+++ app/src/main/java/com/example/ubsandroidapplication/Purchaseconfirmation.java
@@ -1,14 +1,30 @@
-package com.example.ubsandroidapplication;
+package com.example.ubsandroidapplication;;
+
+        import android.content.Context;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Purchaseconfirmation extends AppCompatActivity {
+
+    private ViewGroup drawer;
+    private View fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchaseconfirmation);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_purchaseconfirmation, null, false);
+        drawer.addView(contentView, 0);
+        fab.setVisibility(View.INVISIBLE);
+    }
+    public void goHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

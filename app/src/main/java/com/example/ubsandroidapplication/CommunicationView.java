6c09f385e1c473b671b9e0d46a2db3e00f643b7a
adityamishra <aditya.mishra@mavs.uta.edu>
2021-05-04 12:09:12 -0500

    Updated.

--- app/src/main/java/com/example/ubsandroidapplication/CommunicationView.java
+++ app/src/main/java/com/example/ubsandroidapplication/CommunicationView.java
@@ -1,25 +1,11 @@
 package com.example.ubsandroidapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class CommunicationView extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_communication_view);
-    }
-}
-
-package com.example.ubsandroidapplicationr;
-
         import android.content.Context;
         import android.content.Intent;
         import android.os.Bundle;
         import android.view.LayoutInflater;
         import android.view.View;
+        import android.view.ViewGroup;
         import android.widget.Button;
         import android.widget.EditText;
 
@@ -31,17 +17,18 @@ public class CommunicationView extends MainActivity implements View.OnClickListe
     private EditText editTextMessage;
     //Send button
     private Button buttonSend;
+    private View fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        //inflate your activity layout here!
+
         View contentView = inflater.inflate(R.layout.activity_communication_view, null, false);
-        drawer.addView(contentView, 0);
-        fab.setVisibility(View.INVISIBLE);
-        //Initializing the views
+        //ViewGroup drawer;
+       // drawer.addView(contentView, 0);
+       // fab.setVisibility(View.INVISIBLE);
         editTextEmail = (EditText) findViewById(R.id.editText5);
         editTextSubject = (EditText) findViewById(R.id.editText12);
         editTextMessage = (EditText) findViewById(R.id.editText3);
@@ -49,21 +36,11 @@ public class CommunicationView extends MainActivity implements View.OnClickListe
         //Adding click listener
         buttonSend.setOnClickListener(this);
     }
-
-    private void sendEmail() {
-        //Getting content for email
-        SignInActivity login = new SignInActivity();
-        String email = editTextEmail.getText().toString().trim();
-        String subject = editTextSubject.getText().toString().trim();
-        String message = "University Bazaar" + "\n" + "From: " + login.getUsername() + "\n" + "\n" + editTextMessage.getText().toString().trim();
-        SendMail sm = new SendMail(this, email, subject, message);
-        sm.execute();
-    }
     @Override
     public void onClick(View v) {
-        sendEmail();
-        Intent intent = new Intent(this, EventPage.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
 
